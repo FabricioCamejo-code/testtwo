1bde0cb3c55ede1afe8d49815f590e8a47dc202a
FabricioCamejo-code <fabry_18_rosariio@hotmail.com>
2024-08-10 23:28:41 -0300

    ES6

--- esmodule/index.js
+++ esmodule/index.js
@@ -5,3 +5,5 @@ console.log('El resultado de la resta de ' + n1 + ' menos ' + n2 + ' es: ', oper
 console.log('El resultado de  la suma de ' + n1 + ' mas ' + n2 + ' es: ', operaciones.suma(n1,n2));
 console.log('El resultado de  la division de ' + n1 + ' dividido ' + n2 + ' es: ', operaciones.division(n1,n2));
 console.log('El resultado de  la multiplicacion de ' + n1 + ' por ' + n2 + ' es: ', operaciones.multiplicacion(n1,n2));
+
+console.log('---------------------FabricioCamejo-code----------------------------------------------------')
