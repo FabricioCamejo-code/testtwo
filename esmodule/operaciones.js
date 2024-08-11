23921e7d6ecbeeb24ec985c0156af98f7324e557
FabricioCamejo-code <fabry_18_rosariio@hotmail.com>
2024-08-10 23:26:13 -0300

    Actualizacion

---
+++ esmodule/operaciones.js
@@ -0,0 +1,20 @@
+function resta(a,b){
+    return a - b
+}
+function suma(a,b){
+    return a + b
+}
+function division(a,b){
+    return a / b
+}
+function multiplicacion(a,b){
+    return a * b
+}
+
+
+export default {
+    resta,
+    suma,
+    division,
+    multiplicacion
+}
