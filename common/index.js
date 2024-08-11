2d58f3e643646033be5f7c66d7a22e1bf536f1bf
FabricioCamejo-code <fabry_18_rosariio@hotmail.com>
2024-08-10 23:02:54 -0300

    Inicio Proyecto new

---
+++ common/index.js
@@ -0,0 +1,8 @@
+let operaciones = require('./operaciones')
+
+let n1 = 100 , n2 = 20
+
+console.log('El resultado de la resta de ' + n1 + ' menos ' + n2 + ' es: ', operaciones.resta(n1,n2));
+console.log('El resultado de  la suma de ' + n1 + ' mas ' + n2 + ' es: ', operaciones.suma(n1,n2));
+console.log('El resultado de  la division de ' + n1 + ' dividido ' + n2 + ' es: ', operaciones.division(n1,n2));
+console.log('El resultado de  la multiplicacion de ' + n1 + ' por ' + n2 + ' es: ', operaciones.multiplicacion(n1,n2));
---
+++ common/operaciones.js
@@ -0,0 +1,19 @@
+function resta(a,b){
+    return a - b
+}
+function suma(a,b){
+    return a + b
+}
+function division(a,b){
+    return a / b
+}
+function multiplicacion(a,b){
+    return a * b
+}
+
+module.exports = {
+    resta,
+    suma,
+    division,
+    multiplicacion
+}
